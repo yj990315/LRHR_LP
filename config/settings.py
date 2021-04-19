d64f9cb0c495dcfbf6be3e0e28e527b1c26c5a32
윤준 <june@yunjun-ui-MacBookAir.local>
2021-04-19 14:44:07 +0900

    nginx configuration update

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
