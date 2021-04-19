1ff57efb91e47a24216edeaaeada0d47ce690f55
윤준 <june@yunjun-ui-MacBookAir.local>
2021-04-19 14:53:17 +0900

    static setting

--- config/settings.py
+++ config/settings.py
@@ -125,8 +125,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
