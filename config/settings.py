df9630285b4dcdae9371a2ca1b81339b0420d2b0
윤준 <june@yunjun-ui-MacBookAir.local>
2021-04-21 16:26:53 +0900

    changed to mysql

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ SITE_ID=1
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ihh@0v6f1+69*eh%t()bfq2nvur=m3d*zmpkiumdizbtryq9(l'
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,13 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ my_settings.py
@@ -0,0 +1,12 @@
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'django_insta',                  
+        'USER': 'root',                         
+        'PASSWORD': 'Yj106102!',                 
+        'HOST': 'localhost',                     
+        'PORT': '3306',                          
+    }
+}
+SECRET_KEY = 'django-insecure-ihh@0v6f1+69*eh%t()bfq2nvur=m3d*zmpkiumdizbtryq9(l'
