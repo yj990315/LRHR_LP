68341362bb3d287ab882f41d740f0d6e0234f483
jiwoo <jdev@fapis.io>
2021-06-22 13:52:22 +0900

    랜딩페이지 구성 대공사

--- static/scroll.js
+++ static/scroll.js
@@ -1,10 +1,20 @@
 const scrollDownImg = document.getElementById("chevron-up");
 const logo = document.querySelector(".fapislogo")
+const requestButton = document.getElementById("fixed_request_button")
 
 function scrollDown(){
     let scrollValue = logo.getBoundingClientRect().top;
+    console.log("test")
+    console.log(scrollValue)
     scrollDownImg.style.display = "block"
+    requestButton.style.display = "block"
     if (scrollValue < 0){
         scrollDownImg.style.display = "none";
+        if (scrollValue < -1400){
+            requestButton.style.display = "none"
+            if (scrollValue < -4500){
+                requestButton.style.display = "block"
+            }
+        }
     }
 }
