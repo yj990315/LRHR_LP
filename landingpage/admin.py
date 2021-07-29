2c7d5962bb3c3f076e7c0412022284943eaf7b5f
yj990315 <50268497+yj990315@users.noreply.github.com>
2021-07-29 14:29:40 +0900

    admin page download done

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from .models import Estimate, OverallImage, DetailImage
 
+from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
+
 class OverallImageInline(admin.TabularInline):
     model = OverallImage
 
 class DetailImageInline(admin.TabularInline):
     model = DetailImage
 
-class EstimateAdmin(admin.ModelAdmin):
+class EstimateAdmin(ImportExportMixin, admin.ModelAdmin):
     inlines = (OverallImageInline, DetailImageInline)
     search_fields = ['id', 'name', 'brand']
-    list_display = ['id', 'name', 'brand', 'purpose_of_estimate', 'create_date', 'is_done' ]
+    list_display = ['id', 'name', 'brand', 'type_of_product','create_date', 'is_done' ]
 
 
 admin.site.register(Estimate, EstimateAdmin)
