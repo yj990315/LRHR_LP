5b363868fae2a7487eb9f84a9bc139392c21e467
yj990315 <50268497+yj990315@users.noreply.github.com>
2021-08-02 17:10:10 +0900

    admin update

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Estimate, OverallImage, DetailImage
-
+from import_export import resources, fields
 from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
 
 class OverallImageInline(admin.TabularInline):
@@ -9,8 +9,18 @@ class OverallImageInline(admin.TabularInline):
 class DetailImageInline(admin.TabularInline):
     model = DetailImage
 
+class TypeReport(resources.ModelResource):
+    class Meta:
+       model = Estimate
+
+    product_size = fields.Field(
+        attribute='get_type_of_product_display',
+        column_name='Type Of Product'
+    )
+
 class EstimateAdmin(ImportExportMixin, admin.ModelAdmin):
     inlines = (OverallImageInline, DetailImageInline)
+    resource_class = TypeReport
     search_fields = ['id', 'name', 'brand']
     list_display = ['id', 'name', 'brand', 'type_of_product','create_date', 'is_done' ]
 
@@ -18,3 +28,4 @@ class EstimateAdmin(ImportExportMixin, admin.ModelAdmin):
 admin.site.register(Estimate, EstimateAdmin)
 admin.site.register(OverallImage)
 admin.site.register(DetailImage)
+
