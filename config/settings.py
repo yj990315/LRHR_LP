46b2e9d061f451057ae78832a0c40edace1fe2f7
yj990315 <50268497+yj990315@users.noreply.github.com>
2021-08-06 16:32:41 +0900

    loading

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ import my_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SITE_ID=1
-DEBUG = True
+DEBUG = my_settings.DEBUG
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
---
+++ static/loading.js
@@ -0,0 +1,10 @@
+const spinner = document.getElementById("spinner")
+const button = document.getElementById("next-button")
+const testbutton = document.querySelector(".photo.comment")
+// testbutton.onclick = setSpinner
+
+
+function setSpinner(){
+    console.log("clicked")
+    spinner.style.display = "block"
+}
